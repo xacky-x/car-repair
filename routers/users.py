a4535fe77da3d1cf52c1b8da0db1a8a372e73706
xacky-x <1121479560@qq.com>
2022-05-03 16:34:15 +0800

    增加user的查看接口

--- routers/users.py
+++ routers/users.py
@@ -16,7 +16,31 @@ router = APIRouter(
 )
 
 
+@router.get("/get_all_administrators", response_model=List[schemas.User])
+async def get_all_administrators(skip: int = 0, limit: int = 100, db: Session = Depends(dependencies.get_db)):
+    administrators = crud.get_administrator(db, skip=skip, limit=limit)
+    return administrators
+
+
 @router.get("/get_all_salesmen", response_model=List[schemas.User])
 async def get_all_salesmen(skip: int = 0, limit: int = 100, db: Session = Depends(dependencies.get_db)):
     salesmen = crud.get_salesmen(db, skip=skip, limit=limit)
     return salesmen
+
+
+@router.get("/get_all_maintenances", response_model=List[schemas.User])
+async def get_all_maintenances(skip: int = 0, limit: int = 100, db: Session = Depends(dependencies.get_db)):
+    maintenances = crud.get_maintenances(db, skip=skip, limit=limit)
+    return maintenances
+
+
+@router.get("/get_all_salesmen_and_maintenances", response_model=List[schemas.User])
+async def get_all_salesmen_and_maintenances(skip: int = 0, limit: int = 100, db: Session = Depends(dependencies.get_db)):
+    salesmen_and_maintenances = crud.get_salesman_and_maintenance(db, skip=skip, limit=limit)
+    return salesmen_and_maintenances
+
+
+@router.get("/get_all_users", response_model=List[schemas.User])
+async def get_all_users(skip: int = 0, limit: int = 100, db: Session = Depends(dependencies.get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
