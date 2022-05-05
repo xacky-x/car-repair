f6d7e2656a47e2b69032881914489473c2a7914a
Cetacean <1030wendy@sina.com>
2022-05-05 17:11:19 +0800

    材料表开始

---
+++ models/material.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3.9
+# @Time    : 2022/5/5 17:08
+# @Author  : Cetacean
+# @File    : material.py
+
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+
+from database import Base
+import models
+
+
+class Material(Base):
+    __tablename__ = "material"
+
+    mt_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    mt_name = Column(String(20), nullable=False, comment="材料名称")
+    mt_cost = Column(String(20), comment="材料费")
